d7d2061d9f7f061791a07a571ebf810861794d25
MVBALA <43174220+MVBALA@users.noreply.github.com>
2022-09-19 20:14:53 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array={1,5,3,7,11,9,15,20};
+        System.out.println("Arrray ="+Arrays.toString(array));
+        reverse(array);
+        System.out.println("Reversed Array ="+Arrays.toString(array));
+    }
+    private static void reverse(int[] array){
+       int maxIndex=array.length-1;
+       int halfIndex=array.length/2;
+        for(int i=0;i< halfIndex;i++){
+        int temp=array[i];
+        array[i]=array[maxIndex-i];
+        array[maxIndex-i]=temp;
+        }
     }
 }
